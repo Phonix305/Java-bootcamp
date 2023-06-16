c5c3ddff3346a0f751cbf96d5569dda9a8230534
PHONIX\jyoti <jyotijohn305@gmail.com>
2023-06-16 13:00:17 +0530

    Chorus singing function

---
+++ Chorus.java
@@ -0,0 +1,19 @@
+public class Chorus {
+    public static void main(String[] args) {
+		
+		singChorus();
+		singChorus();
+		singChorus();
+		singChorus();
+		
+    }
+	
+		static void singChorus(){
+	    // Chorus needs to be sung 5 times
+        System.out.println("Don't blame it on the sunshine");
+        System.out.println("Don't blame it on the moonlight");
+        System.out.println("Don't blame it on good times");
+        System.out.println("Don't blame it on the boogie\n");
+			
+	}
+}

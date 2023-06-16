b5bd1357912fb393a8842b87683caf12f09a895d
PHONIX\jyoti <jyotijohn305@gmail.com>
2023-06-16 13:01:21 +0530

    Boolean project

---
+++ Boolean2.java
@@ -0,0 +1,33 @@
+class Boolean2{
+	
+	public static void main(String [] args){
+		 double wallet = 105;
+        double toyCar = 5.99;
+        System.out.println("Can I get this car?");
+		if(wallet >= toyCar){
+			System.out.println("Sure");
+			wallet -= toyCar;
+		}
+		else{
+			System.out.println("Sorry, I only have "+wallet+" left");
+		}
+        //if you have enough money
+            //       print: Sure!
+            //       pay for the toy car 
+        //else:  Sorry, I only have <wallet> left.
+        double nike = 95.99;
+        System.out.println("Can I get these nike shoes?");
+		if(wallet>=nike){
+			System.out.println("Sure");
+			wallet-=nike;
+		}
+		else{
+			System.out.println("Sorry, I only have "+wallet+" left");
+		}
+        
+        //if you have enough money
+            //       print: Sure!
+            //       pay for the nike shoes 
+        //else: Sorry, I only have <wallet> left.}
+}
+}
---
+++ Boolean3.java
@@ -0,0 +1,19 @@
+class Boolean3{
+	
+	public static void main(String [] args){
+		int day=4;
+		boolean holiday=false;
+	
+	if(holiday){
+		System.out.println("Woho No work");
+	}
+	else if(day==6 || day ==7){
+		System.out.println("Woho No work");
+		
+	}
+	else{
+		System.out.println("Wake up at 7:00");
+	}
+	
+	}
+}

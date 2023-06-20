ab8bbd2294f6cf272d0c3c9ae824cccf83303bef
PHONIX\jyoti <jyotijohn305@gmail.com>
2023-06-20 07:42:52 +0530

    use Javapedia.java

---
+++ Javapedia.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Javapedia{
+
+	public static void main(String[] args){
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("How many historical figures will you register?");
+		int no=scan.nextInt();
+		String[][] database=new String[no][3];
+		scan.nextLine();
+		
+		for(int i=0;i<no;i++){
+			System.out.println("     Figure: "+(i+1));
+			
+				
+				System.out.print("      - Name: ");
+				database[i][0]=scan.nextLine();
+				System.out.print("      - Date of birth: ");
+				database[i][1]=scan.nextLine();
+				System.out.print("      - Occupation: ");
+				database[i][2]=scan.nextLine();
+			
+		}
+		
+		
+	}
+	
+	public static void print2DArray(String[][] database,int row,int column){
+	
+	System.out.println("	"+database[row][column]+" ");
+	}
+
+}
---
+++ Workbook13.java
@@ -0,0 +1,13 @@
+class Workbook13{
+	
+	public static void main(String[] args){
+	 
+	 double[][] prices = {
+            {12.99, 8.99, 9.99, 10.49, 11.99},
+            {0.99, 1.99, 2.49, 1.49, 2.99},
+            {8.99, 7.99, 9.49, 9.99, 10.99}
+        };
+		
+		System.out.println("Baking"+ Arrays.tostring(prices[0]));
+	}
+}
